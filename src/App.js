109f56f6fadd440ffc2b26f562672e30a38656a3
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-24 07:13:43 +0200

    :heavy_plus_sign: import layout components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './layouts/Header/Header';
+import Main from './layouts/Main/Main';
+import Footer from './layouts/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
