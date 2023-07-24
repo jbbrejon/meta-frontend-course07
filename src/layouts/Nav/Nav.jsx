42bd22e9d4b88d869c7ce1005c550149a14fe92b
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-24 07:12:31 +0200

    :construction: Create Nav component

---
+++ src/layouts/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
