615469f7313c408d4d58d7e4fcc40deea32c0b4d
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-28 20:56:27 +0200

    :heavy_plus_sign: Import CSS module

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,10 +1,15 @@
+// Import components
 import Nav from "../Nav/Nav"
 
+// Import assets
 import logo from '../../assets/brand/Logo.svg'
+
+// Import CSS module
+import styles from './Header.module.css'
 
 function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <img src={logo} alt="Little Lemon logo" />
             <Nav />
         </header>
