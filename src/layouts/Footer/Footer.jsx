2d6e20afc6c63965126ac90c9c19c4f8d926287b
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-29 19:53:09 +0200

    :heavy_plus_sign: Import FooterItem component

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,6 +1,29 @@
+// Import components
+import FooterItem from '../../components/FooterItem/FooterItem'
+
+// Import assets
+import logo from '../../assets/brand/Logo-footer.png'
+
+// Import CSS module
+import styles from './Footer.module.css'
+
 function Footer() {
+
+    const navigationList = ["Home", "About", "Menu", "Reservations", "Order Online", "Login"];
+    const contactList = ["Address", "Phone Number", "Email"];
+    const socialMediaList = ["Facebook", "Instragram", "Thread"];
+
     return (
-        <footer>Footer</footer>
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <img className={styles.logo} src={logo} alt="Footer logo" />
+                <div className={styles.items}>
+                    <FooterItem title="Navigation" list={navigationList} />
+                    <FooterItem title="Contact" list={contactList} />
+                    <FooterItem title="Social Medias" list={socialMediaList} />
+                </div>
+            </div>
+        </footer>
     )
 }
 
