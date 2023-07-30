949e5c48df0a56f6446a3be0c0c8eb61855f1ac3
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-30 19:32:12 +0200

    :construction: Add page title

--- src/pages/BookingPage/BookingPage.jsx
+++ src/pages/BookingPage/BookingPage.jsx
@@ -1,9 +1,13 @@
 // Import components
 import BookingForm from "../../components/BookingForm/BookingForm"
 
+// Import CSS module
+import styles from './BookingPage.module.css'
+
 function BookingPage() {
     return (
         <main>
+            <h1 className={styles.title}>Reservations</h1>
             <BookingForm />
         </main>
     )
