196cf49b486aa1f71ee8d85cc3b6886e41231086
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-30 09:51:55 +0200

    :construction: Rename page Reservation page

---
+++ src/pages/BookingPage/BookingPage.jsx
@@ -0,0 +1,12 @@
+// Import components
+import BookingForm from "../../components/BookingForm/BookingForm"
+
+function BookingPage() {
+    return (
+        <main>
+            <BookingForm />
+        </main>
+    )
+}
+
+export default BookingPage
