de64acbbe83077ae5ab7aec4108cd1f47a3f3a8b
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-30 11:11:41 +0200

    :heavy_plus_sign: Import WeekSpecials component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 // Import components
 import Cta from "../../components/Cta/Cta"
+import WeekSpecials from "../../components/WeekSpecials/WeekSpecials"
 
 function Home() {
     return (
         <main>
             <Cta />
+            <WeekSpecials />
         </main>
     )
 }
