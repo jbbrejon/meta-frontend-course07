32895744857a5ccb57f18f50467e4ba38425fdf5
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-30 11:10:11 +0200

    :construction: Create SpecialItem component

---
+++ src/components/SpecialItem/SpecialItem.jsx
@@ -0,0 +1,27 @@
+// Import dependencies
+import { Link } from 'react-router-dom'
+
+// Import CSS module
+import styles from './SpecialItem.module.css'
+
+function SpecialItem({ meal }) {
+    return (
+        <article className={styles.article}>
+            <div className={styles.card}>
+                <img src={meal.image} alt={meal.name} />
+            </div>
+            <div className={styles.cardHeader}>
+                <h3>{meal.name}</h3>
+                <span>{meal.price}</span>
+            </div>
+            <div className={styles.cardFooter}>
+                <p>{meal.description}</p>
+                <Link to="/order">
+                    Order a delivery + icon
+                </Link>
+            </div>
+        </article>
+    )
+}
+
+export default SpecialItem
