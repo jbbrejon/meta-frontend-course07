2349870d7135e7f447744b2f5213a1d8d7a6e072
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-30 13:44:18 +0200

    :heavy_plus_sign: Import Testimonials component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 // Import components
-import Cta from "../../components/Cta/Cta"
-import WeekSpecials from "../../components/WeekSpecials/WeekSpecials"
+import Cta from "../../components/Cta/Cta";
+import WeekSpecials from "../../components/WeekSpecials/WeekSpecials";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 function Home() {
     return (
         <main>
             <Cta />
             <WeekSpecials />
+            <Testimonials />
         </main>
     )
 }
