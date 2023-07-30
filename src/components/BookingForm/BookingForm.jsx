7a4931e90d338d23ede39c5cd334e57a9d3e50fb
Jean-Baptiste BREJON <jbbrejon.dev@gmail.com>
2023-07-30 17:30:20 +0200

    :sparkles: improve form validation and add operations on form submission

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,8 +1,10 @@
 // Import dependencies
 import { useState } from 'react'
+import { submitAPI } from '../../utils/api/mockApi';
 
 // Import components
 import Button from '../Button/Button';
+import Confirmation from '../Confirmation/Confirmation';
 
 // Import CSS module
 import styles from './BookingForm.module.css'
@@ -17,23 +19,33 @@ function BookingForm() {
     }
     // useState instance for reservation properties
     const [reservation, setReservation] = useState(initialState);
+    // useState for submission status
+    const [isSubmitted, setisSubmitted] = useState(false);
+
+    // Toogle confirmation modal
+    function toggleModal() {
+        console.log("test")
+        setisSubmitted(!isSubmitted)
+    }
 
     // Time options
     const availableTimes = ["17:00", "18:00", "19:00", "21:00", "22:00"]
-    const timeOptions = availableTimes.map((time) => <option>{time}</option>)
+    const timeOptions = availableTimes.map((time) => <option key={time}>{time}</option>)
 
     // Update reservation properties
     function changeHandler(e) {
-        console.log(e)
         setReservation({ ...reservation, [e.target.name]: e.target.value });
     }
 
     // Submit form
     function submitHandler(e) {
         e.preventDefault();
-        console.log(reservation);
+        // Send Data to API
+        submitAPI(reservation);
         // Reset reservation state
         setReservation(initialState);
+        // Display confirmation
+        setisSubmitted(true);
     }
 
     function getIsFormValid() {
@@ -46,28 +58,31 @@ function BookingForm() {
     }
 
     return (
-        <form className={styles.form} onSubmit={submitHandler}>
-            <label className={styles.label} htmlFor="date" >Date</label>
-            <input className={styles.input} type="date" id="date" name="date" title="date" value={reservation.date} onChange={changeHandler} />
-            <label className={styles.label} htmlFor="time">Time</label>
-            <select className={styles.input} id="time" name="time" title="time" value={reservation.time} onChange={changeHandler}>
-                <option value="" disabled hidden>Select time</option>
-                {timeOptions}
-            </select>
-            <label className={styles.label} htmlFor="numberOfGuests">Number of Guests</label>
-            <input className={styles.input} type="number" id="numberOfGuests" name="numberOfGuests" title="numberOfGuests" placeholder="Choose number of guests" min="1" max="10" value={reservation.numberOfGuests} onChange={changeHandler} />
-            <label className={styles.label} htmlFor="occasion">Occasion</label>
-            <select className={styles.input} id="occasion" name="occasion" title="occasion" value={reservation.occasion} onChange={changeHandler}>
-                <option value="" disabled hidden>Select occasion</option>
-                <option>Birthday</option>
-                <option>Anniversary</option>
-            </select>
-            <Button
-                type="submit"
-                status={!getIsFormValid()}
-                valid="Make Your reservation"
-                invalid="Please fill in all fields" />
-        </form>
+        <>
+            <form className={styles.form} onSubmit={submitHandler}>
+                <label className={styles.label} htmlFor="date" >Date</label>
+                <input className={styles.input} type="date" id="date" name="date" title="date" value={reservation.date} onChange={changeHandler} />
+                <label className={styles.label} htmlFor="time">Time</label>
+                <select className={styles.input} id="time" name="time" title="time" value={reservation.time} onChange={changeHandler}>
+                    <option value="" disabled hidden>Select time</option>
+                    {timeOptions}
+                </select>
+                <label className={styles.label} htmlFor="numberOfGuests">Number of Guests</label>
+                <input className={styles.input} type="number" id="numberOfGuests" name="numberOfGuests" title="numberOfGuests" placeholder="Choose number of guests" min="1" max="10" value={reservation.numberOfGuests} onChange={changeHandler} />
+                <label className={styles.label} htmlFor="occasion">Occasion</label>
+                <select className={styles.input} id="occasion" name="occasion" title="occasion" value={reservation.occasion} onChange={changeHandler}>
+                    <option value="" disabled hidden>Select occasion</option>
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </select>
+                <Button
+                    type="submit"
+                    status={!getIsFormValid()}
+                    valid="Make Your reservation"
+                    invalid="Please fill in all fields" />
+            </form>
+            {isSubmitted ? <Confirmation toggle={toggleModal} /> : null}
+        </>
     )
 }
 
